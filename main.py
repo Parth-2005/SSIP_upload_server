a1405c26ea4db0c427a8b39061031ead042ae94e
Parth-2005 <76589211+Parth-2005@users.noreply.github.com>
2024-04-02 21:32:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-from flask import Flask, request, jsonify
-from flask_pymongo import MongoClient
+from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from getmac import get_mac_address
-from flask_cors import CORS
+import json
+from flask import Flask, jsonify
+from flask_socketio import SocketIO
+
+mongo = MongoClient("mongodb+srv://23bit009:uTIJOIdpKFXOn8PL@cluster0.eltoend.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+
+db = mongo['weatherdb']
+cities = db.cities
 
 app = Flask(__name__)
-CORS(app)
-app.config["SECRET_KEY"]="SECRET"
-mongo = MongoClient("mongodb+srv://nasaapplication:password123parthandnasaapplication@cluster0.kq6h2lm.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 
-mongo = mongo["SSIP"]
-Students = mongo.students
+socketio = SocketIO(app)
 
+@socketio.on('search')
+def search(data):
+    city = data['city']
+    #search with regex expression
+    result = cities.find({"name": {"$regex": f".*{city}.*", "$options": "i"}}).limit(5)
+    socketio.emit('search_result', {'data': list(result)})
 
-@app.route('/insertData', methods=['POST'])
-def insert_data():
-    try:
-        # Get data from the request
-        data = request.get_json()
-
-        # Ensure 'roll' and 'MAC' are present in the request data
-        if 'roll' not in data:
-            return jsonify({'error': 'Roll and MAC are required'}), 400
-
-        # Insert data into MongoDB
-        mac = get_mac_address(ip=request.remote_addr)
-        Students.find_one_and_update({"roll": data["roll"]}, {'$set': {"mac" : mac}})
-
-        return jsonify({'message': 'Data inserted successfully'}), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+if __name__ == '__main__':
+    socketio.run(app)
